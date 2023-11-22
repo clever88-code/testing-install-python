a19ba1513ea21cbaec6f5914e528b968b4e93d81
clever88 <prostopavel2020@yandex.ru>
2023-11-22 11:18:24 +0800

    fix error

--- main.py
+++ main.py
@@ -6,27 +6,9 @@ print("Версия Python:")
 print(sys.version)
 print()
 
-# 2. Проверка установки библиотеки NumPy
+# 2. Проверка установки библиотеки Requests
 try:
     print('ТЕСТ 1')
-    import numpy as np
-    print("Библиотека NumPy установлена. Версия:", np.__version__)
-except ImportError:
-    print("Библиотека NumPy не установлена. Установите, используя 'pip install numpy'")
-print()
-
-# 3. Проверка установки библиотеки Matplotlib
-try:
-    print('ТЕСТ 2')
-    import matplotlib.pyplot as plt
-    print("Библиотека Matplotlib установлена. Версия:", plt.__version__)
-except ImportError:
-    print("Библиотека Matplotlib не установлена. Установите, используя 'pip install matplotlib'")
-print()
-
-# 4. Проверка установки библиотеки Requests
-try:
-    print('ТЕСТ 3')
     import requests
     response = requests.get("https://www.python.org")
     if response.status_code == 200:
@@ -37,15 +19,27 @@ except ImportError:
     print("Библиотека Requests не установлена. Установите, используя 'pip install requests'")
 except Exception as e:
     print("Произошла ошибка при выполнении запроса:", e)
-
-# 5. Проверка установки библиотеки pandas
-try:
-    print('ТЕСТ 4')
-    import pandas as pd
-    print("Библиотека pandas установлена. Версия:", pd.__version__)
-except ImportError:
-    print("Библиотека pandas не установлена. Установите, используя 'pip install pandas'")
 print()
 
+# 3. Проверка установки библиотеки random
+try:
+    print('ТЕСТ 2')
+    import random
+    print("Библиотека random установлена. Случайное число:", random.random())
+except ImportError:
+    print("Библиотека random не установлена. Установите, используя 'pip install random'")
+except Exception as e:
+    print("Произошла ошибка при использовании библиотеки random:", e)
+print()
 
-# Дополнительные тесты могут быть добавлены в зависимости от ваших потребностей.
+# 4. Проверка установки библиотеки Django
+try:
+    print('ТЕСТ 3')
+    import django
+    print("Библиотека Django установлена. Версия:", django.get_version())
+except ImportError:
+    print("Библиотека Django не установлена. Установите, используя 'pip install django'")
+except Exception as e:
+    print("Произошла ошибка при импорте Django:", e)
+print()
+
